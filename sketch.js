8bbdb4bf7fa70a1ed8310ac3793666856859ad54
Pronav05 <pforpranav05@gmail.com>
2020-12-22 17:37:42 +0800

    virtual pet 2

--- sketch.js
+++ sketch.js
@@ -1,57 +1,97 @@
+var database ,dog,dog1,dog2
+var position
+//var form
+var feed,add
+var foodobject
+var Feedtime
+var Lastfeed
 //Create variables here
-var dog, dogImage
-var happyDog
-var database
-var foodS
-var foodStock
 
 function preload()
+
 {
-  //load images here
-  dogImage = loadImage("images/Dog.png");
-  happyDog = loadImage("images/happydog.png");
+  dogimg1 = loadImage("images/dogImg.png")
+  dogimg2 = loadImage("images/dogImg1.png")
+	//load images here
 }
 
 function setup() {
-  createCanvas(500, 500);
-  dog = createSprite(250,250,10,10);
-  dog.addImage("dog", dogImage);
-  dog.scale = 0.5;
-  dog.addImage("dog2", happyDog);
-  dog.scale = 0.5;
-
+	createCanvas(1000, 500);
   database = firebase.database();
+  console.log(database);
+ 
+  foodobject=new Food()
+  dog = createSprite(550,250,10,10);
+  dog.addImage(dogimg1)
+  dog.scale=0.2
+  
+ 
 
-  foodStock = database.ref('Food');
-  foodStock.on("value", readStock);
-}
+  var dogo = database.ref('Food');
+  dogo.on("value", readPosition, showError);
+feed = createButton("FEED DRAGO")
+feed.position(500,15)
+feed.mousePressed(FeedDog)
+add = createButton("ADD FOOD")
+add.position(400,15)
+add.mousePressed(AddFood)
 
-
-function draw() {  
-  background(46, 139, 87);
-
-  if(keyWentDown(UP_ARROW)){
-    foodS = foodS - 1;
-    writeStock(foodS);
-    dog.changeImage("dog2", happyDog);
-  }
-
-  drawSprites();
-  //add styles here
-  textSize(12);
-  fill("black");
-  text(foodS, 20, 20);
-}
-
-function readStock(data){
-  foodS = data.val();
-}
-
-function writeStock(x){
-  database.ref('/').update({
-    Food:x
-  })
 } 
 
 
 
+function draw(){
+ { background(46,139,87);
+ foodobject.display()
+ 
+ }
+ drawSprites();
+  
+  fill(255,255,254);
+ textSize(15);
+
+  // text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+ 
+  //add styles here
+drawSprites();
+}
+function readPosition(data){
+  position = data.val();
+  foodobject.updateFoodStock(position)
+  console.log(position.x);
+  
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
+
+function writePosition(nazo){
+  if(nazo>0){
+    nazo=nazo-1
+  }
+  else{
+    nazo=0
+  }
+  database.ref('/').set({
+    'Food': nazo
+  })
+
+}
+function AddFood(){
+position++
+database.ref('/').update({
+  Food:position
+}
+
+)
+}
+function FeedDog(){
+
+dog.addImage(dogimg2)
+foodobject.updateFoodStock(foodobject.getFoodStock()-1)
+ database.ref('/').update({
+   Food:foodobject.getFoodStock(),
+   FeedTime:hour ()
+ })
+}
